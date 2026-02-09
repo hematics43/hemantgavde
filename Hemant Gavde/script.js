27d48e8f8cbece80faa1bbcec928cbc3b2d4bfa6
Hemant Gavde <hemantgavde2@gmail.com>
2026-02-09 13:05:45 +0530

    Delete Hemant Gavde directory

--- Hemant Gavde/script.js
+++
@@ -1,6 +0,0 @@
-// Theme Toggle
-const themeToggle = document.getElementById('theme-toggle');
-themeToggle.addEventListener('click', ()=>{
-  document.body.classList.toggle('dark');
-  themeToggle.textContent = document.body.classList.contains('dark') ? '☀️ Light Mode' : '🌙 Dark Mode';
-});
