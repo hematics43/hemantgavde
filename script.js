5d3f0699a6a59e12f846ecc566f7f30b7d4c2399
Hemant Gavde <hemantgavde2@gmail.com>
2026-03-25 16:48:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ themeToggle.addEventListener('click', ()=>{
   document.body.classList.toggle('dark');
   themeToggle.textContent = document.body.classList.contains('dark') ? '☀️ Light Mode' : '🌙 Dark Mode';
 });
+ const text = document.querySelector(".colorful-text");
+  const words = text.innerText.split(" ");
+
+  text.innerHTML = words.map(word => `<span>${word}</span>`).join(" ");
